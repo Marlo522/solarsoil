f9f2df194bdfacf7d6718e3af43474875e853205
carlmarinyo <marinojerome2@gmail.com>
2026-03-10 00:07:58 +0800

    feat: create controller and add product function for farmer

---
+++ app/Controllers/FarmerController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\ProductModel;
+
+class FarmerController extends BaseController
+{
+    public function addProduct()
+    {
+        $productModel = new ProductModel();
+
+        $image = $this->request->getFile('image');
+        $imageName = null;
+
+        if ($image && $image->isValid() && !$image->hasMoved()) {
+
+            $imageName = $image->getRandomName();
+
+            $image->move(ROOTPATH . 'public/uploads/products', $imageName);
+        }
+
+        $data = [
+            
+            'name' => $this->request->getPost('name'),
+            'category' => $this->request->getPost('category'),
+            'price' => $this->request->getPost('price'),
+            'stock_quantity' => $this->request->getPost('stock_quantity'),
+            'description' => $this->request->getPost('description'),
+            'image' => $imageName
+        ];
+
+        $productModel->insert($data);
+
+        return redirect()->to(base_url('farmer/dashboard'));
+    }
+}
